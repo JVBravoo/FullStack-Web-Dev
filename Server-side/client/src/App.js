1a10342892c561e10d90764aa0f80ff8e9f575d5
João Victor Bravo <bravo@MacBook-Pro-de-Joao.local>
2021-06-22 15:51:46 -0300

    update: section 6

--- Server-side/client/src/App.js
+++ Server-side/client/src/App.js
@@ -4,20 +4,16 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h2>
+          Hello World.
+        </h2>
+      </div>
+        <p
+          className="App-link">
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="/auth/google"> Sign in with Google</a>
     </div>
   );
 }
